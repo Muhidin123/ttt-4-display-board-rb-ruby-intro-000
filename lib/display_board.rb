ff6bc7950a7578852a198eacf5b1d6edc87fd5ae
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-08-24 19:33:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+board = [" ", " ", " ", " ", " ", " ", " ", " "," "]
+
 def display_board(board)
-  puts "   |   |   "
+  puts ""#{board[0]}"   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
